52fbce4859f54555764a9a3ca171c7bae8ce8c9e
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-03-05 11:57:53 -0700

    Add legend and horizontal gridlines

--- ganttcharter.py
+++ ganttcharter.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.dates import SU, DateFormatter, WeekdayLocator
@@ -18,6 +19,16 @@ COLOR_TABLE = {
     "magenta": "#c27ba0",
 }
 
+BIOIN_BLUE = "#333399"
+
+# HATCH_TABLE = {
+#     "Steven": "/",
+#     "Robyn": "\\",
+#     "Both": "x",
+#     np.nan: "",
+# }
+
+matplotlib.rcParams.update({'font.size': 14})
 
 def gantt_chart(csv_file: Path, fig_file: Path, current_date=pd.Timestamp.today()):
     """
@@ -47,12 +58,20 @@ def gantt_chart(csv_file: Path, fig_file: Path, current_date=pd.Timestamp.today(
     df["task_duration"] = df["days_to_end"] - df["days_to_start"] + 1  # to include also the end date
 
     df["_color"] = df["color"].map(COLOR_TABLE)
+    # df["_hatch"] = df["label"].map(HATCH_TABLE)
 
     fig, ax = plt.subplots(figsize=(16, 9))
     # increase left padding
     plt.subplots_adjust(left=0.2)
 
-    ax.barh(y=df["title"], width=df["task_duration"], left=df["start_date"], color=df["_color"])
+    ax.barh(
+        y=df["title"],
+        width=df["task_duration"],
+        left=df["start_date"],
+        color=df["_color"],
+        label=df["label"],
+        # hatch=df["_hatch"],
+    )
 
     # change x-axis labels to dates
     ax.xaxis_date()
@@ -61,17 +80,32 @@ def gantt_chart(csv_file: Path, fig_file: Path, current_date=pd.Timestamp.today(
     ax.xaxis.set_major_locator(WeekdayLocator(byweekday=SU))
 
     # add vertical line for current date
-    ax.axvline(x=current_date, color="blue", linestyle="-")
-    # add label above vertical line with current date
+    ax.axvline(x=current_date, color=BIOIN_BLUE, linestyle="-")
+    # add text above vertical line with current date outside of the plot
+    # ax.text(current_date, 0, "Today", rotation=90, verticalalignment="bottom", horizontalalignment="right")
+    date_ratio = (current_date - df["start_date"].min()) / (df["end_date"].max() - df["start_date"].min())
+    plt.gcf().text(date_ratio - 0.025, 0.89, current_date.strftime("%b %d"), fontsize=14, color=BIOIN_BLUE)
+
     ax.set_xlabel("Date")
     ax.set_ylabel("Task")
     # ax.set_title("Gantt Chart")
+
     # horizontal lines
-    # ax.grid(axis="y")
-    # axes[0].grid(axis='y', which='minor')  # set the grid on the minor ticks
-    # axes[0].tick_params(axis='y', which='minor', length=0)  # hide minor tick marks
+    number_bars = len(df['title'].unique())
+    ax.set_yticks(list(map(lambda y: y - 0.5, range(number_bars + 1))), minor=True)
+    ax.set_ylim(-0.5, number_bars - 0.5)
+    ax.grid(axis='y', which='minor', zorder=0)  # set the grid on the minor ticks
+    ax.tick_params(axis='y', which='minor', length=0)  # hide minor tick marks
+
+    # add legend
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    by_label = {k: v for k, v in by_label.items() if k != "nan"}
+    plt.legend(by_label.values(), by_label.keys(), loc="lower right")
+
     ax.xaxis.grid(True)
-    plt.savefig(fig_file)
+    plt.savefig(fig_file, bbox_inches="tight")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a Gantt chart from a CSV file.")
